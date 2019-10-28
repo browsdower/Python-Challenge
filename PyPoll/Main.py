efe0c895648c7d784cb5fa9e27ae66b64ea19955
browsdower <53028521+browsdower@users.noreply.github.com>
2019-10-28 11:57:35 -0500

    poll csv

---
+++ PyPoll/Main.py
@@ -0,0 +1,17 @@
+import os
+import csv
+#how to define the folder path?
+#provide attributes and it will create
+csvpath = os.path.join("..", "PyPoll", "election_data.csv")
+
+
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    print(csvreader) 
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)
