50675459fd157320b691128e9a45f8691736e1a2
browsdower <53028521+browsdower@users.noreply.github.com>
2019-10-29 15:06:47 -0500

    summary prints

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -25,3 +25,28 @@ with open(csvpath, newline="") as csvfile:
 
 total_months = len(months)
 print(total_months)
+
+greatest_inc = revenue[0]
+greatest_dec = revenue[0]
+total_revenue = 0
+
+#loop through revenue and compare to find greatest inc and dec
+#add revenue to total revenue
+for r in range(len(revenue)):
+    if revenue[r] >= greatest_inc:
+        greatest_inc = revenue[r]
+        great_inc_month = months[r]
+    elif revenue[r] <= greatest_dec:
+        greatest_dec = revenue[r]
+        great_dec_month = months[r]
+    total_revenue += revenue[r]
+
+print(f'Greatest Decrease ${greatest_dec}')
+
+print(f'Greatest Decrease Month {great_dec_month}')
+
+print(f'Greatest Increase ${greatest_inc}')
+
+print(f'Greatest Increase Month {great_inc_month}')
+
+print(f'Total ${total_revenue}')
