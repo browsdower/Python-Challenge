fa750f4f8a2624036ab8c1110292c6db471c0b92
browsdower <53028521+browsdower@users.noreply.github.com>
2019-10-30 11:11:42 -0500

    final code

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,12 +1,13 @@
 import os
 import csv
-#how to define the folder path?
-#provide attributes and it will create
+#follow path, then method, then folder, then file name
 csvpath = os.path.join("..", "PyBank", "budget_data.csv")
 
+#create variables to hold month and revenue totals
 months = []
 revenue = []
 
+#run open and read file with header
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
@@ -15,37 +16,43 @@ with open(csvpath, newline="") as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-
+#format csv data into list format
     for row in csvreader:
         print(row)
 
         months.append(row[0])
         revenue.append(int(row[1]))
 
+#begin summary output
+print("Financial Analysis:")
 
+#use len to get the total count of a column
 total_months = len(months)
-print(total_months)
+print(f'Total Months: {total_months}')
 
-greatest_inc = revenue[0]
-greatest_dec = revenue[0]
+#define variables for increase, decrease and total revenue amounts
+greatest_increase = revenue[0]
+greatest_decrease = revenue[0]
 total_revenue = 0
 
-#loop through revenue and compare to find greatest inc and dec
+#set loop for revenue, total revenue
+#compare revenue to find greatest increase and decrease
 #add revenue to total revenue
 for r in range(len(revenue)):
-    if revenue[r] >= greatest_inc:
-        greatest_inc = revenue[r]
+    if revenue[r] >= greatest_increase:
+        greatest_increase = revenue[r]
         great_inc_month = months[r]
-    elif revenue[r] <= greatest_dec:
-        greatest_dec = revenue[r]
+    elif revenue[r] <= greatest_decrease:
+        greatest_decrease = revenue[r]
         great_dec_month = months[r]
     total_revenue += revenue[r]
 
-print(f'Greatest Decrease ${greatest_dec}')
+#final print commands for summary 
+print(f'Greatest Decrease ${greatest_decrease}')
 
 print(f'Greatest Decrease Month {great_dec_month}')
 
-print(f'Greatest Increase ${greatest_inc}')
+print(f'Greatest Increase ${greatest_increase}')
 
 print(f'Greatest Increase Month {great_inc_month}')
 
